a2ca329fe017b704ebd606066a84b6427a78ec2b
mhallaq <malhallaq@gmail.com>
2023-02-21 22:15:42 +0000

    Update dbConnection.js

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "cyf",
-  host: "localhost",
-  port: 5432,
-  database: "youtubevideos",
+  password: "8ZxpCfuEEvwV3TDJirhg",
+  host: "containers-us-west-145.railway.app",
+  port: 7022,
+  database: "railway",
 });
 
 module.exports = pool;
